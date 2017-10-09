deb3225b38c45eb45fecb5159db5bdc30214155b
venkatraobolla <venkatrao.bolla@thinkartha.com>
2017-10-09 11:57:24 +0530

    Updated Scripts

--- src/test/java/exe/ADS_Automation_Demo.java
+++ src/test/java/exe/ADS_Automation_Demo.java
@@ -1,8 +1,8 @@
-    /* 
+/* 
     	Author: Artha Data Solutions Testing Team
     	Date: 2017-10-04
     	Description: This code Will Automate 13 Test cases  for Customer, Audit, Payment & Partners
-    */
+ */
 
 //Package Statement
 package exe;
@@ -40,16 +40,16 @@ public class ADS_Automation_Demo
 	//public static int Noc;
 	Connection con;
 	Statement stmt;
-		
+
 	//Database Connection Statement
 	@BeforeMethod
 	public void DB_Details() throws ClassNotFoundException, SQLException 
-	  {
+	{
 		Class.forName("com.mysql.jdbc.Driver");
-		  con=DriverManager.getConnection("jdbc:mysql://localhost:3306/arthadb","root","mysql"); 
-		  stmt=con.createStatement(); 
-	  }
-	
+		con=DriverManager.getConnection("jdbc:mysql://localhost:3306/arthadb","root","mysql"); 
+		stmt=con.createStatement(); 
+	}
+
 	/*
 	 * Customer Name Check
 	 * 			1. Here we are Validating Both First & Last Name
@@ -57,37 +57,37 @@ public class ADS_Automation_Demo
 	 * 			3. Both Names Allow Only Alphabets (a to z / A to Z)
 	 */
 	@Test(priority=1)
-	public void ArthaDB_Customer_First_Last_Name_Check_TC001() throws SQLException, RowsExceededException, WriteException, IOException 
+	public void ArthaDB_Customer_FirstName_Check_TC001() throws SQLException, RowsExceededException, WriteException, IOException 
 	{
-			  StringBuffer TC009r=new StringBuffer();
-				  try
-				  { 
-					  int Noc=0; 
-					  ResultSet rs=stmt.executeQuery("SELECT * FROM customers WHERE FirstName RLIKE '[[:<:]][a-z][[:>:]]' or LastName RLIKE '[[:<:]][a-z][[:>:]]' or FirstName is null or LastName is null"); 
-					  List<String> TC009rs=new ArrayList<String>();
-					  while (rs.next()) 
-					  {
-						  Noc=rs.getRow();
-						  TC009rs.add(rs.getString("SSN"));
-						  
-					  }
-					  if(Noc==0)
-						  {
-					        Assert.assertEquals("CUSTOMERS-First & Last Name check is PASSED", 0, Noc);
-						  }
-						  else
-						  {
-								for(int i=0;i<TC009rs.size();i++)
-								{
-									  TC009r.append(TC009rs.get(i)+","); 	  
-								}
-						        Assert.assertEquals("CUSTOMERS-First & Last Name check is Failed at SSN="+TC009r, 0, Noc);
-						  }  
-				  }
-				  catch (Exception e1) 
-				  {
-						  e1.printStackTrace();
-				  } 
+		StringBuffer TC009r=new StringBuffer();
+		try
+		{ 
+			int Noc=0; 
+			ResultSet rs=stmt.executeQuery("select * from customers where FirstName regexp '^[^A-Za-z]' or FirstName is null"); 
+			List<String> TC009rs=new ArrayList<String>();
+			while (rs.next()) 
+			{
+				Noc=rs.getRow();
+				TC009rs.add(rs.getString("SSN"));
+
+			}
+			if(Noc==0)
+			{
+				Assert.assertEquals("CUSTOMERS-FirstName check is PASSED", 0, Noc);
+			}
+			else
+			{
+				for(int i=0;i<TC009rs.size();i++)
+				{
+					TC009r.append(TC009rs.get(i)+","); 	  
+				}
+				Assert.assertEquals("CUSTOMERS-FirstName check is Failed at SSN="+TC009r, 0, Noc);
+			}  
+		}
+		catch (Exception e1) 
+		{
+			e1.printStackTrace();
+		} 
 	}
 
 	/*
@@ -98,38 +98,38 @@ public class ADS_Automation_Demo
 	@Test(priority=2)
 	public void ArthaDB_Customers_SSN_Check_TC002() throws SQLException, RowsExceededException, WriteException, IOException 
 	{
-			 StringBuffer TC010r=new StringBuffer();
-				  try
-				  { 
-					  int Noc=0; 
-					  ResultSet rs=stmt.executeQuery("SELECT * FROM customers WHERE SSN not RLIKE '[[:<:]][0-9]{3}[[:>:]](-)[[:<:]][0-9]{2}[[:>:]](-)[[:<:]][0-9]{4}[[:>:]]' or SSN is null"); 
-					  List<String> TC010rs=new ArrayList<String>();
-					  while (rs.next()) 
-					  {
-						  Noc=rs.getRow();
-						  TC010rs.add(rs.getString("CustomerPK"));
-						  
-					  }
-					  if(Noc==0)
-						  {
-					        Assert.assertEquals("CUSTOMERS- SSN check is PASSED", 0, Noc);
-					        
-						  }
-						  else
-						  {
-								for(int i=0;i<TC010rs.size();i++)
-								{
-									  TC010r.append(TC010rs.get(i)+","); 
-								}
-						        Assert.assertEquals("CUSTOMERS- SSN check is Failed at CustomerPK="+TC010r, 0, Noc);
-						  }  
-				  }
-				  catch (Exception e1) 
-				  {
-						  e1.printStackTrace();
-				  } 
+		StringBuffer TC010r=new StringBuffer();
+		try
+		{ 
+			int Noc=0; 
+			ResultSet rs=stmt.executeQuery("SELECT * FROM customers WHERE SSN not RLIKE '[[:<:]][0-9]{3}[[:>:]](-)[[:<:]][0-9]{2}[[:>:]](-)[[:<:]][0-9]{4}[[:>:]]' or SSN is null"); 
+			List<String> TC010rs=new ArrayList<String>();
+			while (rs.next()) 
+			{
+				Noc=rs.getRow();
+				TC010rs.add(rs.getString("CustomerPK"));
+
+			}
+			if(Noc==0)
+			{
+				Assert.assertEquals("CUSTOMERS- SSN check is PASSED", 0, Noc);
+
+			}
+			else
+			{
+				for(int i=0;i<TC010rs.size();i++)
+				{
+					TC010r.append(TC010rs.get(i)+","); 
+				}
+				Assert.assertEquals("CUSTOMERS- SSN check is Failed at CustomerPK="+TC010r, 0, Noc);
+			}  
+		}
+		catch (Exception e1) 
+		{
+			e1.printStackTrace();
+		} 
 	}
-	
+
 	/*
 	 * Customer Phone number is mandatory and it is in specified format (for example +1 333 333 4444)
 	 * Input Excel Phone number does not contain PLUS (+) symbol, it contain hyphen(-)
